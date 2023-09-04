de7a350422fa737226348c2894518c9ad166ec7d
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2023-09-04 15:21:45 +0200

    feat(controller, service): add ApplicationUser controller & service

---
+++ src/main/java/ch/zli/m223/controller/ApplicationUserController.java
@@ -0,0 +1,72 @@
+package ch.zli.m223.controller;
+
+import ch.zli.m223.model.ApplicationUser;
+import ch.zli.m223.service.ApplicationUserService;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("/users")
+@Tag(name = "Users", description = "Handling of users.")
+public class ApplicationUserController {
+
+  @Inject
+  ApplicationUserService applicationUserService;
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Returns all users.", description = "Returns all users.")
+  @RolesAllowed("admin")
+  public List<ApplicationUser> index() {
+    return applicationUserService.findAll();
+  }
+
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  @PermitAll
+  @Operation(
+    summary = "Creates a new user.",
+    description = "Creates a new user."
+  )
+  public ApplicationUser create(@Valid ApplicationUser applicationUser) {
+    applicationUser.setPassword(applicationUser.getPassword());
+    return applicationUserService.createApplicationUser(applicationUser);
+  }
+
+  @DELETE
+  @Operation(summary = "Deletes a user.", description = "Deletes a user.")
+  @Path("/{id}")
+  @RolesAllowed("admin")
+  public void delete(@PathParam("id") long id) {
+    applicationUserService.deleteApplicationUser(id);
+  }
+
+  @PUT
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Updates a user.", description = "Updates a user.")
+  @Path("/{id}")
+  @RolesAllowed("admin")
+  public ApplicationUser update(
+    @Valid ApplicationUser applicationUser,
+    @PathParam("id") long id
+  ) {
+    applicationUser.setPassword(applicationUser.getPassword());
+    applicationUser.setId(id);
+    return applicationUserService.updateApplicationUser(applicationUser);
+  }
+}
---
+++ src/main/java/ch/zli/m223/service/ApplicationUserService.java
@@ -0,0 +1,82 @@
+package ch.zli.m223.service;
+
+import ch.zli.m223.model.ApplicationUser;
+import ch.zli.m223.model.RoleEnum;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.NotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class ApplicationUserService {
+
+  @Inject
+  EntityManager entityManager;
+
+  @Transactional
+  public ApplicationUser createApplicationUser(
+    ApplicationUser applicationUser
+  ) {
+    CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+    cq.select(cb.count(cq.from(ApplicationUser.class)));
+    var count = entityManager.createQuery(cq).getSingleResult();
+
+    if (count == 0) {
+      applicationUser.setRole(RoleEnum.ADMIN);
+    } else {
+      applicationUser.setRole(RoleEnum.MEMBER);
+    }
+
+    entityManager.persist(applicationUser);
+    return applicationUser;
+  }
+
+  @Transactional
+  public void deleteApplicationUser(Long id) {
+    var applicationUser = entityManager.find(ApplicationUser.class, id);
+    if (applicationUser == null) {
+      throw new NotFoundException();
+    }
+    applicationUser.getBookings().forEach(b -> entityManager.remove(b));
+    entityManager.remove(applicationUser);
+  }
+
+  @Transactional
+  public ApplicationUser updateApplicationUser(
+    ApplicationUser newApplicationUser
+  ) {
+    entityManager.merge(newApplicationUser);
+    return newApplicationUser;
+  }
+
+  public List<ApplicationUser> findAll() {
+    var query = entityManager.createQuery(
+      "FROM ApplicationUser",
+      ApplicationUser.class
+    );
+    return query.getResultList();
+  }
+
+  public Optional<ApplicationUser> findByEmail(String email) {
+    var user = entityManager
+      .createNamedQuery("ApplicationUser.findByEmail", ApplicationUser.class)
+      .setParameter("email", email)
+      .getResultStream()
+      .findFirst();
+    return user;
+  }
+
+  public ApplicationUser findById(long id) {
+    var user = entityManager.find(ApplicationUser.class, id);
+    if (user == null) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+}
