0bab37ce82780c28dd1da2df409599fdd7a09f89
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2023-09-04 16:03:14 +0200

    feat(controller, service): add Feedback controller & service

---
+++ src/main/java/ch/zli/m223/controller/FeedbackController.java
@@ -0,0 +1,68 @@
+package ch.zli.m223.controller;
+
+import ch.zli.m223.model.Feedback;
+import ch.zli.m223.service.ApplicationUserService;
+import ch.zli.m223.service.FeedbackService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
+import java.util.List;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("/feedbacks")
+@Tag(name = "Feedbacks", description = "Handling of feedbacks.")
+@RolesAllowed({ "member", "admin" })
+public class FeedbackController {
+
+  @Inject
+  FeedbackService feedbackService;
+
+  @Inject
+  ApplicationUserService userService;
+
+  @Inject
+  @RequestScoped
+  SecurityContext ctx;
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Operation(
+    summary = "Get all feedbacks.",
+    description = "Returns all feedbacks."
+  )
+  public List<Feedback> index() {
+    return feedbackService.findAll();
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Get a feedback.", description = "Return one feedback.")
+  @Path("/{id}")
+  public Feedback show(@PathParam("id") long id) {
+    return feedbackService.findById(id);
+  }
+
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Operation(
+    summary = "Create a feedback.",
+    description = "Creates a new feedback."
+  )
+  public Feedback create(@Valid Feedback feedback) {
+    var user = userService.findByEmail(ctx.getUserPrincipal().getName());
+    assert user.isPresent();
+    feedback.setApplicationUser(user.get());
+    return feedbackService.createFeedback(feedback);
+  }
+}
---
+++ src/main/java/ch/zli/m223/service/FeedbackService.java
@@ -0,0 +1,50 @@
+package ch.zli.m223.service;
+
+import ch.zli.m223.model.Feedback;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.NotFoundException;
+import java.util.List;
+
+@ApplicationScoped
+public class FeedbackService {
+
+  @Inject
+  EntityManager entityManager;
+
+  public List<Feedback> findAll() {
+    var query = entityManager.createQuery("FROM Feedback", Feedback.class);
+    return query.getResultList();
+  }
+
+  public Feedback findById(long id) {
+    var feedback = entityManager.find(Feedback.class, id);
+    if (feedback == null) {
+      throw new NotFoundException();
+    }
+    return feedback;
+  }
+
+  @Transactional
+  public Feedback createFeedback(Feedback feedback) {
+    entityManager.persist(feedback);
+    return feedback;
+  }
+
+  @Transactional
+  public Feedback updateFeedback(Feedback newfeedback) {
+    entityManager.merge(newfeedback);
+    return newfeedback;
+  }
+
+  @Transactional
+  public void deleteFeedback(Long id) {
+    var feedback = entityManager.find(Feedback.class, id);
+    if (feedback == null) {
+      throw new NotFoundException();
+    }
+    entityManager.remove(feedback);
+  }
+}
